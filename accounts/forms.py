6b38c926d5bc8bba1aab9ef44f59e8852689f474
Flavio Macedo <fhenrique609@gmail.com>
2026-03-19 01:03:08 -0300

    Criando a pagina de login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,31 +1,31 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from .models import Address
 
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(
-        required=True,
-        widget=forms.EmailInput(attrs={
+        required = True,
+        widget = forms.EmailInput(attrs={
             'class': 'block w-full px-4 py-3 rounded-lg text-gray-600 bg-indigo-50 border border-indigo-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-600 outline-none transition',
             'placeholder': 'seu@email.com',
         })
     )
 
     password1 = forms.CharField(
-        label="Senha",
-        widget= forms.PasswordInput(attrs={
+        label ="Senha",
+        widget = forms.PasswordInput(attrs={
             'class': 'block w-full px-4 py-3 rounded-lg text-gray-600 bg-indigo-50 border border-indigo-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-600 outline-none transition',
             'placeholder': '••••••••',
         })
     )
 
     password2 = forms.CharField(
-        label="Confirmação de senha",
-        widget=forms.PasswordInput(attrs={
+        label = "Confirmação de senha",
+        widget = forms.PasswordInput(attrs={
             'class': 'block w-full px-4 py-3 rounded-lg text-gray-600 bg-indigo-50 border border-indigo-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-600 outline-none transition',
-            'placeholder': 'Confirme sua senha'
+            'placeholder': 'Confirme sua senha',
         })
     )
 
@@ -35,9 +35,39 @@ class RegisterForm(UserCreationForm):
         widgets = {
             'username': forms.TextInput(attrs={
                 'class': 'block w-full px-4 py-3 rounded-lg text-gray-600 bg-indigo-50 border border-indigo-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-600 outline-none transition',
-                'placeholder': 'Digite seu usuário'
+                'placeholder': 'Digite seu usuário',
             }),
         }
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label = "Usuário",
+        widget = forms.TextInput(attrs={
+            'class': 'block w-full px-4 py-3 rounded-lg text-gray-600 bg-indigo-50 border border-indigo-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-600 outline-none transition',
+            'placeholder': 'Digite seu usuário',
+            'autofocus': True,
+        })
+    )
+
+    password = forms.CharField(
+        label="Senha",
+        widget=forms.PasswordInput(attrs={
+            'class': 'block w-full px-4 py-3 rounded-lg text-gray-600 bg-indigo-50 border border-indigo-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-600 outline-none transition',
+            'placeholder': '••••••••',
+        })
+    )
+
+    remember = forms.BooleanField(
+        label="Lembrar de mim",
+        required=False,
+        widget=forms.CheckboxInput(attrs={
+            'class': 'h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
 
 class AddressForm(forms.ModelForm):
     class Meta:
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from . import forms
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path(
+        'login/',
+        LoginView.as_view(
+            template_name='accounts/login.html', 
+            authentication_form=forms.CustomLoginForm,
+            redirect_authenticated_user=True,
+            next_page='home',
+        ), 
+        name='login'
+    ),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
     path('cart/', views.cart_view, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
 ]
